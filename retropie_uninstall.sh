50ef42d1f9a372627dc7d0231222a372b2f6e9d4
mafe72 <mafe72@users.noreply.github.com>
2021-06-29 18:30:26 -0500

    Unistall script for Raspbian and RetroPie

---
+++ retropie_uninstall.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#Un-install script For Raspbian and RetroPie
+
+#Step 1) Check if root--------------------------------------
+if [[ $EUID -ne 0 ]]; then
+   echo "Please execute script as root." 
+   exit 1
+fi
+#-----------------------------------------------------------
+
+CNF=/boot/config.txt
+SCRIPTS=/home/pi/scripts
+DIR=/home/pi/picontrol
+
+
+#-----------------------------------------------------------
+#Step 2) Remove Installation directory ---------------------
+
+sudo rm -r $DIR
+sudo rm -r $SCRIPTS
+
+#-----------------------------------------------------------
+
+#-----------------------------------------------------------
+#Step 4) Reboot to apply changes----------------------------
+echo "NESPi piControl Board un-install complete. Rebooting after 3 seconds."
+sleep 4
+sudo reboot
+#-----------------------------------------------------------
